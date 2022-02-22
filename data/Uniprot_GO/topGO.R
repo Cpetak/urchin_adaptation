a6f63415dc41fc9161ab613c510685cfcabc4d04
Csenge Petak <csenge.petak@uvm.edu>
2022-02-22 10:30:16 -0500

    FCT 2 pop chi and GO results

--- data/Uniprot_GO/topGO.R
+++ data/Uniprot_GO/topGO.R
@@ -4,13 +4,13 @@ setwd("~/Desktop/urchin_adaptation/data/Uniprot_GO")
 geneID2GO <- readMappings("GO_mapping_topGO") # uniprot to GO mapping
 geneNames <- names(geneID2GO)
 
-myInterestingGenes <- read.csv("temp_files_for_GO/uniprotIDs_prom_locs.txt", header = FALSE) # list of interesting genes, output of LOC to uniprot mapping
+myInterestingGenes <- read.csv("temp_files_for_GO/uniprotIDs_non_prom_locs.txt", header = FALSE) # list of interesting genes, output of LOC to uniprot mapping
 intgenes <- myInterestingGenes[, "V1"]
 geneList <- factor(as.integer(geneNames %in% intgenes)) # mask of 0 and 1 if geneName is interesting
 names(geneList) <- geneNames # geneList but annotated with the gene names
 
 GOdata <- new("topGOdata", 
-              ontology = "MF", # ontology of interest (BP, MF or CC)
+              ontology = "CC", # ontology of interest (BP, MF or CC)
               allGenes = geneList,
               annot = annFUN.gene2GO, 
               gene2GO = geneID2GO)
@@ -18,7 +18,9 @@ GOdata <- new("topGOdata",
 resultFisher <- runTest(GOdata, algorithm = "classic", statistic = "fisher") # these are the options I'll be using! checked!
 resultFisher
 
-allRes <- GenTable(GOdata, classicFisher = resultFisher, topNodes = 10) # top 10 enriched terms
+allRes <- GenTable(GOdata, classicFisher = resultFisher, topNodes = 6) # top 10 enriched terms
 allRes
+write.csv(allRes,"non_prom_locs_results_CC.csv", row.names = FALSE)
 
-showSigOfNodes(GOdata, score(resultFisher), firstSigNodes = 10, useInfo = 'all') 
+
+#showSigOfNodes(GOdata, score(resultFisher), firstSigNodes = 10, useInfo = 'all') 
